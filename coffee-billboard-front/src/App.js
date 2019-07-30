ab73f24170dd3e7a3dada54ec2bb8ce725f2cffc
SimasKi <simaskizys@gmail.com>
2019-07-30 20:15:21 +0300

    Add front end project

---
+++ coffee-billboard-front/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import CoffeeEntry from './components/CoffeeEntry'
+import './App.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Coffee billboard</h1>  
+    </div>
+  );
+}
+
+export default App;
