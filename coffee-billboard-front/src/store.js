6df9c7068dc9338a10c9f1f7a3b710841bfd6287
SimasKi <simaskizys@gmail.com>
2019-08-03 16:54:44 +0300

    Fix import

--- coffee-billboard-front/src/store.js
+++ coffee-billboard-front/src/store.js
@@ -1,6 +1,6 @@
 import { createStore, compose, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
-import rootReducer from "./rootReducer";
+import { rootReducer } from "./rootReducer";
 
 export const store = createStore(
   rootReducer,
