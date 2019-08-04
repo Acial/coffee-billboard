209b671c20d83f88c04cb29c788a0260c485dfc8
SimasKi <simaskizys@gmail.com>
2019-08-04 17:31:37 +0300

    Properly format component

--- coffee-billboard-front/src/components/coffee-billboard/CoffeeCard.js
+++ coffee-billboard-front/src/components/coffee-billboard/CoffeeCard.js
@@ -12,21 +12,19 @@ const CoffeeCard = ({ id, title, imageUrl, price }) => {
   };
 
   const content = (
-          <div className='col-lg-3 col-md-3'>
-            <div className='card-type-2'>
-              <div className='img-c'>
-                <img
-                  src={imageUrl}
-                  width='800'
-                  height='800'
-                />
-              </div>
-              <div className='clear-line' />
-              <h4>{title}</h4>
-              <p>Price: {price}$</p>
-              <button className='btn btn-primary' onClick={handleRemove}>Remove</button>
-            </div>
-          </div>
+    <div className='col-lg-3 col-md-3'>
+      <div className='card-type-2'>
+        <div className='img-c'>
+          <img src={imageUrl} width='800' height='800' />
+        </div>
+        <div className='clear-line' />
+        <h4>{title}</h4>
+        <p>Price: {price}$</p>
+        <button className='btn btn-primary' onClick={handleRemove}>
+          Remove
+        </button>
+      </div>
+    </div>
   );
 
   return content;
