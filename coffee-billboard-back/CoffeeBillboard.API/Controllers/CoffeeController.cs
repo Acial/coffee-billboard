80efda33c7a827ccb7f85ef0054a8fd008e5b855
SimasKi <simaskizys@gmail.com>
2019-08-04 13:31:33 +0300

    Create coffee controller

---
+++ coffee-billboard-back/CoffeeBillboard.API/Controllers/CoffeeController.cs
@@ -0,0 +1,28 @@
+﻿using CoffeeBillboard.DataAccess;
+using CoffeeBillboard.DataAccess.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoffeeBillboard.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CoffeeController
+    {
+        private readonly ICoffeeRepository _coffeeRepository;
+
+        public CoffeeController(ICoffeeRepository coffeeRepository)
+        {
+            _coffeeRepository = coffeeRepository;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Coffee>>> Get()
+        {
+            return await _coffeeRepository.GetCoffees();
+        }
+    }
+}
